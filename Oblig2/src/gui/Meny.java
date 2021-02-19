94e21ea1481fd3c71689ea19f91ee162c4bab525
emilieh.fisketjon <emilieh.fisketjon@emilies-mbp-2.lan>
2021-02-19 17:52:28 +0100

    wegt

--- Oblig2/src/gui/Meny.java
+++ Oblig2/src/gui/Meny.java
@@ -1,7 +1,40 @@
 package gui;
 
-public class Meny {
-	
-	
+import java.util.Scanner;
 
+import no.hvl.dat102.adt.FilmarkivADT;
+
+public class Meny
+{
+	private Tekstgrensesnitt ui;
+
+	
+	public Meny()
+	{
+		ui = new Tekstgrensesnitt();
+	}
+
+	public void start()
+	{
+		while (true)
+		{
+			int choice = ui.showMainMenu();
+
+			switch (choice)
+			{
+			case 1: ui.searchForAvailableCars();
+					break;
+
+			case 2: ui.searchForReservations();
+					break;
+
+			case 3: ui.exit();
+					break;
+
+			default:
+					System.out.println("Error: The input is not a valid choice");
+					break;
+			}
+		}
+	}
 }
---
+++ Oblig2/src/gui/Tekstgrensesnitt.java
@@ -0,0 +1,154 @@
+package gui;
+
+import java.util.List;
+import java.util.Scanner;
+
+import car.Car;
+import car.Company;
+import car.Customer;
+import car.Reservation;
+import no.hvl.dat102.adt.FilmarkivADT;
+
+public class Tekstgrensesnitt
+{
+
+	Company company;
+	Scanner sc;
+	
+	public Tekstgrensesnitt()
+	{
+		sc = new Scanner(System.in);
+		sc.useDelimiter(System.lineSeparator());
+	}
+
+	public int showMainMenu()
+	{
+		int choice = 0;
+
+		System.out.println("Welcome, to Abu Tallah's Rental Company");
+		System.out.println("1: Search for available cars");
+		System.out.println("2: Search for reservations.");
+		System.out.println("3: Exit the program.");
+
+		String input = sc.next();
+
+		try
+		{
+			choice = Integer.parseInt(input);
+			return choice;
+		} catch (NumberFormatException e)
+		{
+			System.out.println("Error: The input is not a valid choice");
+			e.printStackTrace();
+			return 0;
+		}
+	}
+	
+	
+	
+	public void searchForAvailableCars() {
+		
+		System.out.println("Please enter the city you want to rent from 'Bergen' or 'Oslo'.");
+		String city = sc.next();
+		
+		Compan
+		
+		
+	}
+
+	public void searchForReservations() {
+		
+		System.out.println("Please enter the surname to find the reservation");
+		String surname = sc.next();
+		System.out.println("Please enter the producer of the film you want to add.");
+		String producer = sc.next();
+		int year;
+		while (true)
+		{
+			System.out.println("Please enter the year the movie was released.");
+
+			String input = sc.next();
+			try
+			{
+				year = Integer.parseInt(input);
+				break;
+			} catch (NumberFormatException e)
+			{
+				continue;
+			}
+		}
+		System.out.println("Please enter the company that made the film.");
+		String company = sc.next();
+		System.out.println("Please enter the genre of the film.");
+		String genreString = sc.next();
+		Sjanger genre = Sjanger.findGenre(genreString);
+
+		Film newFilm = new Film(0, year, producer, title, company, genre);
+		System.out.println("Film successfully saved to the archive.");
+
+		return newFilm;
+		
+	}
+	
+	
+	
+	
+	
+
+	public void printAvailableCars(List<Car> cars){
+		for(Car c : cars) {
+			System.out.println(c.toString());
+		}
+	}
+	
+	public void findAvailableCars() {
+		
+		ledigeBiler();
+	}
+	
+	public void choseCar(Car car, int number) {
+		
+		
+		
+		
+	}
+	
+	public Reservation readReservation()
+	{
+		System.out.println("Please enter the title of the film you want to add.");
+		String title = sc.next();
+		System.out.println("Please enter the producer of the film you want to add.");
+		String producer = sc.next();
+		int year;
+		while (true)
+		{
+			System.out.println("Please enter the year the movie was released.");
+
+			String input = sc.next();
+			try
+			{
+				year = Integer.parseInt(input);
+				break;
+			} catch (NumberFormatException e)
+			{
+				continue;
+			}
+		}
+		System.out.println("Please enter the company that made the film.");
+		String company = sc.next();
+		System.out.println("Please enter the genre of the film.");
+		String genreString = sc.next();
+		Sjanger genre = Sjanger.findGenre(genreString);
+
+		Film newFilm = new Film(0, year, producer, title, company, genre);
+		System.out.println("Film successfully saved to the archive.");
+
+		return newFilm;
+	}
+
+	public void exit()
+	{
+		System.out.println("Shutting down...");
+		System.exit(0);
+	}
+}
