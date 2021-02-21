671e6ca3e7df009027a70d148671c235f75ce2f3
emilieh.fisketjon <emilieh.fisketjon@emilies-mbp-2.lan>
2021-02-21 16:06:51 +0100

    test

---
+++ Oblig2/src/Test/TestClass.java
@@ -0,0 +1,36 @@
+package Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class TestClass {
+
+	@BeforeEach
+	void setUp() {
+	}
+	
+	@Test
+	void availableCarsTest() {
+		
+	}
+	
+	@Test
+	void findReservationTest() {
+		
+	}
+	
+	@Test
+	void priceTest() {
+		
+		
+		
+	}
+	
+	@Test
+	void pickCarTest() {
+		
+	}
+
+}
